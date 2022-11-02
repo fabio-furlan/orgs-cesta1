16a68f57a96f380d3e49f4ef083c73b7c572d566
Fabio Furlan <furlan.fabio@hotmail.com>
2022-11-01 21:26:54 -0300

    v01

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StatusBar, SafeAreaView } from 'react-native';
 
-import Cesta from './src/telas/Cesta';
+import Cesta from './src/telas /Cesta';
 
 export default function App() {
   return (
